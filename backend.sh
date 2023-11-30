3631c7999b257a6263b864c281fd86542ccf0a5d
Ubuntu <ubuntu@ip-172-31-33-172.ap-south-1.compute.internal>
2023-11-30 07:57:38 +0000

    Backend.sh added

---
+++ backend.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir backend
+cd backend
+touch backend.py
+echo "I am Bushra Maryam. Why so serious?!">backend.py
+git add .
+git commit -m "Backend folder and sh file created"
+git push origin main
